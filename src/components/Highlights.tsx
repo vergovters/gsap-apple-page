a5eb19e63edbf52aea159c88a1e7013236e00a38
illia <fialkivskiji@gmail.com>
2024-05-09 13:30:13 +0300

    carousel

--- src/components/Highlights.tsx
+++ src/components/Highlights.tsx
@@ -1,6 +1,7 @@
 import { useGSAP } from "@gsap/react"
 import gsap from "gsap"
 import { rightImg, watchImg } from "../utils"
+import VideoCarousel from "./VideoCarousel"
 
 
 const Highlights = () => {
@@ -26,6 +27,7 @@ const Highlights = () => {
             </p>
           </div>
         </div>
+        <VideoCarousel/>
       </div>
     </section>
   )
